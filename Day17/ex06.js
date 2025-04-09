c4fe6b01c21d1a40afd6dbd762745b1f5cbdfc09
hoangnm-ndm <hoangnm.ndm@gmail.com>
2025-04-09 17:21:02 +0700

    Day 17: reduce

---
+++ Day17/ex06.js
@@ -0,0 +1,9 @@
+const animals = ["dog", "cat", "python", "cat", "virusS", "snake", "virusS", "panda", "đom đóm", "virusS"];
+
+/**
+ * Tạo ra function trả về mảng mới đã loại bỏ các phần tử trùng lặp ở mảng đưa vào.
+ */
+
+function duplicateFilter(arr) {
+	return arr.reduce((acc, cur) => {}, []);
+}
