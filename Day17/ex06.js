39646b5ed1c1e920b4f29c04d596f6572939fd8d
hoangnm-ndm <hoangnm.ndm@gmail.com>
2025-04-11 15:29:08 +0700

    Day 18

--- Day17/ex06.js
+++ Day17/ex06.js
@@ -1,9 +1,17 @@
-const animals = ["dog", "cat", "python", "cat", "virusS", "snake", "virusS", "panda", "đom đóm", "virusS"];
+const animals = ["dog", "dog", "cat", "python", "cat", "virusS", "snake", "virusS", "panda", "đom đóm", "virusS"];
 
 /**
  * Tạo ra function trả về mảng mới đã loại bỏ các phần tử trùng lặp ở mảng đưa vào.
  */
 
 function duplicateFilter(arr) {
-	return arr.reduce((acc, cur) => {}, []);
+	return arr.reduce((acc, cur) => {
+		// * if (acc.findIndex((item) => item === cur) === -1) {
+		if (!acc.includes(cur)) {
+			acc.push(cur);
+		}
+		return acc;
+	}, []);
 }
+
+console.log(duplicateFilter(animals));
