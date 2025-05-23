f9f4e54b811c67ef4c91919329d6d1c8610c3156
hoangnm-ndm <hoangnm.ndm@gmail.com>
2025-05-23 15:40:02 +0700

    Day06: Component form

--- src/pages/admin/ProductForm.jsx
+++ src/pages/admin/ProductForm.jsx
@@ -1,16 +1,6 @@
 import React, { useState } from "react";
-import styled from "@emotion/styled";
 import { createProduct } from "../../api/productApi";
-
-const Form = styled.form`
-	padding: 24px;
-	margin: 0 auto;
-	border: 1px solid #ccc;
-	border-radius: 8px;
-	box-shadow: 0 0 12px rgba(0, 0, 0, 0.5);
-	width: 400px;
-	max-width: 100%;
-`;
+import FormCommon from "../../components/FormCommon";
 
 const initFormData = {
 	title: "",
@@ -42,7 +32,7 @@ const ProductForm = () => {
 	return (
 		<>
 			{/* <h1>Add Product</h1> */}
-			<Form action="" onSubmit={handleSubmit}>
+			<FormCommon handleSubmit={handleSubmit}>
 				<div className="mb-3">
 					<label className="form-label" htmlFor="" name="title">
 						Title
@@ -73,7 +63,7 @@ const ProductForm = () => {
 				<div className="mb-3">
 					<button className="btn btn-primary w-100">Add Product</button>
 				</div>
-			</Form>
+			</FormCommon>
 		</>
 	);
 };
