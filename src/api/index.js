c879c9ad0675db63ae0c5e3832e09951a42a6886
hoangnm-ndm <hoangnm.ndm@gmail.com>
2025-05-28 14:49:21 +0700

    fix: update baseURL

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-	baseURL: "http://localhost:3000",
+	// baseURL: "http://localhost:3000",
+	baseURL: "https://codefarm-json-server-web01-6mnx.onrender.com/",
 	headers: {
 		"Content-Type": "application/json",
 	},
