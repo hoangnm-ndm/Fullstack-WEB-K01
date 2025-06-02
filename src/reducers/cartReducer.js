22cab9d74726c5216897df056c319612a1c7efa5
hoangnm-ndm <hoangnm.ndm@gmail.com>
2025-06-02 16:32:06 +0700

    decrement, increment

--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -15,12 +15,21 @@ export default function cartReducer(state, action) {
 		case "REMOVE_CART":
 			return { ...state, cart: state.cart.filter((item) => item.id !== action.payload.id) };
 
-		case "INCREMENT":
-			return;
+		case "INCREMENT_CART":
+			return {
+				...state,
+				cart: state.cart.map((item) =>
+					item.id === action.payload.id ? { ...item, quantity: item.quantity + 1 } : item
+				),
+			};
 
-		case "DECREMENT":
-			return;
-
+		case "DECREMENT_CART":
+			return {
+				...state,
+				cart: state.cart.map((item) =>
+					item.id === action.payload.id ? Math.max(1, { ...item, quantity: item.quantity - 1 }) : item
+				),
+			};
 		default:
 			return state;
 	}
